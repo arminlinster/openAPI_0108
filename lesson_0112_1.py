02ce168d276c521bde32636537be070457853ea3
Armin Lin 2024 <arminlinster@gmail.com>
2024-01-12 09:31:32 +0800

    0112b M

--- lesson_0112_1.py
+++ lesson_0112_1.py
@@ -4,4 +4,14 @@ n = 0
 while value <= 100:
     n += value
     value += 1
+print("第一種 while")
 print(n)
+value = 1
+total = 0
+while True:
+    total += value
+    value += 1
+    if value > 100:
+        break
+print("第二種 while")
+print(total)
