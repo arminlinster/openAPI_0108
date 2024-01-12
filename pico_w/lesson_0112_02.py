025cb994b7598bd09bd80ae6bf2fc1f16040bc16
Armin Lin 2024 <arminlinster@gmail.com>
2024-01-12 14:25:56 +0800

    0112 PM m 1

---
+++ pico_w/lesson_0112_02.py
@@ -0,0 +1,12 @@
+import machine
+import time
+from machine import Pin
+led25 = Pin("LED", Pin.OUT)
+i = 0;
+while True:
+    i += 1
+    print(machine.freq() , "delay" , i)
+    led25.value(1)
+    time.sleep(1)
+    led25.value(0)
+    time.sleep(1)
