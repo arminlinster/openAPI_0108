3075a87a177c56ff5237d8b35f6d29a72ee4111e
Armin Lin 2024 <arminlinster@gmail.com>
2024-01-19 11:59:56 +0800

    0119c upd armin 1159

--- openAPI/main.py
+++ openAPI/main.py
@@ -13,4 +13,5 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-print('hello kitty')
+#print(read_root())
+#print('hello kitty')
