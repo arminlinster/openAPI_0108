7cec0528e06452e11f0eefb7bee0236bee6957b1
Armin Lin 2024 <arminlinster@gmail.com>
2024-01-19 16:11:33 +0800

    0119 1611 m

--- openAPI/main.py
+++ openAPI/main.py
@@ -1,19 +1,23 @@
-from typing import Union
-import os
 import redis
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 app = FastAPI()
+load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 
 @app.get("/")
 def read_root():
-    counter = redis_conn.incrby("test123",1)
+    counter = redis_conn.incr("test123",1)
     return {"Counter": counter}
 
+@app.get("/counter/{c}")
+def counter(c:int):
+    counter = redis_conn.incr('test123',c)
+    return {"Counter": counter}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
-#print(read_root())
-#print('hello kitty')
+
